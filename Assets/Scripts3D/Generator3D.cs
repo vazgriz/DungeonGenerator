ac4d6c1025ab9bb9fc4a9c811f5e9eed499cb20c
james-schiele <jamesschiele@hotmail.com>
2021-08-30 19:53:57 +0100

    add prefabs to test

--- Assets/Scripts3D/Generator3D.cs
+++ Assets/Scripts3D/Generator3D.cs
@@ -41,6 +41,13 @@ public class Generator3D : MonoBehaviour {
     [SerializeField]
     Material greenMaterial;
 
+    [SerializeField]
+    GameObject floorPrefab;
+    [SerializeField]
+    GameObject corridorPrefab;
+    [SerializeField]
+    GameObject stairsPrefab;
+
     Random random;
     Grid3D<CellType> grid;
     List<Room> rooms;
@@ -235,10 +242,28 @@ public class Generator3D : MonoBehaviour {
         }
     }
 
+    // Floor prefab
     void PlaceCube(Vector3Int location, Vector3Int size, Material material) {
-        GameObject go = Instantiate(cubePrefab, location, Quaternion.identity);
+        // GameObject go = Instantiate(cubePrefab, location, Quaternion.identity);
+        GameObject go = Instantiate(floorPrefab, location, Quaternion.identity);
         go.GetComponent<Transform>().localScale = size;
-        go.GetComponent<MeshRenderer>().material = material;
+        // go.GetComponent<MeshRenderer>().material = material;
+    }
+
+    // Corridor prefab
+    void PlaceCorridorPrefab(Vector3Int location, Vector3Int size, Material material) {
+    // GameObject go = Instantiate(cubePrefab, location, Quaternion.identity);
+    GameObject go = Instantiate(corridorPrefab, location, Quaternion.identity);
+    go.GetComponent<Transform>().localScale = size;
+    // go.GetComponent<MeshRenderer>().material = material;
+    }
+
+    // Stairs prefab
+    void StairsPrefab(Vector3Int location, Vector3Int size, Material material) {
+    // GameObject go = Instantiate(cubePrefab, location, Quaternion.identity);
+    GameObject go = Instantiate(stairsPrefab, location, Quaternion.identity);
+    go.GetComponent<Transform>().localScale = size;
+    // go.GetComponent<MeshRenderer>().material = material;
     }
 
     void PlaceRoom(Vector3Int location, Vector3Int size) {
@@ -246,10 +271,11 @@ public class Generator3D : MonoBehaviour {
     }
 
     void PlaceHallway(Vector3Int location) {
-        PlaceCube(location, new Vector3Int(1, 1, 1), blueMaterial);
+        //PlaceCube(location, new Vector3Int(1, 1, 1), blueMaterial);
+        PlaceCorridorPrefab(location, new Vector3Int(1, 1, 1), blueMaterial);
     }
 
     void PlaceStairs(Vector3Int location) {
-        PlaceCube(location, new Vector3Int(1, 1, 1), greenMaterial);
+        StairsPrefab(location, new Vector3Int(1, 1, 1), greenMaterial);
     }
 }

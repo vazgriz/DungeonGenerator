f09b137386c46587f79dd76688b6ad04991c5f42
james-schiele <>
2021-09-29 23:07:19 +0100

    re-formatting of levelassetgenerator and generator3d to instantiate placeholder cubes in former

--- Assets/LevelAssetGenerator.cs
+++ Assets/LevelAssetGenerator.cs
@@ -5,16 +5,62 @@ using UnityEngine;
 public class LevelAssetGenerator : MonoBehaviour
 {
 
+    [SerializeField]
+    Material redMaterial;
+    string roomTag = "Room";
+    [SerializeField]
+    Material blueMaterial;
+    string hallTag = "Hall";
+    [SerializeField]
+    Material greenMaterial;
+    string stairsTag = "Stairs";
+
+    [SerializeField]
+    GameObject floorPrefab;
+    [SerializeField]
+    GameObject corridorPrefab;
+    [SerializeField]
+    GameObject stairsPrefab;
+
+    public GameObject cubePrefab;
+
     void Start()
     {
         // Search for and add all GameObjects (cubes) marked with a scene tag to an array
-        GameObject[] rooms = GameObject.FindGameObjectsWithTag("Room");
-        GameObject[] halls = GameObject.FindGameObjectsWithTag("Halls");
-        GameObject[] stairs = GameObject.FindGameObjectsWithTag("Stairs");
+        // GameObject[] halls = GameObject.FindGameObjectsWithTag("Halls");
+        // GameObject[] stairs = GameObject.FindGameObjectsWithTag("Stairs");
     }
 
     void Update()
-    {
-        
+    {   
     }
+
+    public void PlaceRoom(Vector3Int location, Vector3Int size)
+    {
+        PlaceCube(location, size, redMaterial, roomTag);
+    }
+    public void PlaceHallway(Vector3Int location)
+    {
+        PlaceCube(location, new Vector3Int(1, 1, 1), blueMaterial, hallTag);
+    }
+    public void PlaceStairs(Vector3Int location)
+    {
+        PlaceCube(location, new Vector3Int(1, 1, 1), greenMaterial, stairsTag);
+    }
+
+    private void PlaceCube(Vector3Int location, Vector3Int size, Material material, string tag)
+    {
+        GameObject go = Instantiate(cubePrefab, location, Quaternion.identity);
+        go.GetComponent<Transform>().localScale = size;
+        go.GetComponent<MeshRenderer>().material = material;
+        go.tag = tag;
+    }
+
+
+
+
+
+
+
+
 }

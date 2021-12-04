530c8222fc83870190e3ee9ec0a0fbe3ca742609
lrgmincher <lomy.mincher@decision.tech>
2021-12-04 15:19:43 +0000

    Structure building logic -> StructureBuilder

--- Assets/Level/Builders/RoomBuilder.cs
+++ Assets/Level/Builders/RoomBuilder.cs
@@ -2,7 +2,6 @@
 using Assets.World.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace Assets.Level.Builders
@@ -28,13 +27,12 @@ namespace Assets.Level.Builders
         [SerializeField]
         GameObject eastEdge;
 
-        [SerializeField]
-        GameObject _ceiling;
-        [SerializeField]
-        GameObject _floor;
-        [SerializeField]
-        GameObject _wall;
+        private StructureBuilder _structureBuilder;
 
+        public void Awake()
+        {
+            _structureBuilder = FindObjectOfType<StructureBuilder>();
+        }
 
         public void BuildMany(ICollection<Room> rooms)
         {
@@ -64,70 +62,12 @@ namespace Assets.Level.Builders
             {
                 for (var z = zOrigin; z - zOrigin < depth; z++)
                 {
-                    BuildFloor(x, yOrigin, z);
-                    BuildCeiling(x, yOrigin + height, z);                    
+                    _structureBuilder.BuildFloor(x, yOrigin, z);
+                    _structureBuilder.BuildCeiling(x, yOrigin + height, z);                    
                 }
             }
 
-            BuildWalls(xOrigin, yOrigin, zOrigin, width, height, depth);
-        }
-
-        private void BuildFloor(int x, int y, int z)
-        {
-            Instantiate(_floor, new Vector3(x + 0.5f, y, z + 0.5f), Quaternion.identity);
-        }
-
-        private void BuildCeiling(int x, int y, int z)
-        {
-            Instantiate(_ceiling, new Vector3(x + 0.5f, y, z + 0.5f), Quaternion.identity);
-        }
-
-        private void BuildWalls(int xOrigin, int yOrigin, int zOrigin, int width, int height, int depth)
-        {
-            // Build North/South wall
-            for (var x = xOrigin; x < xOrigin + width; x++)
-            {
-                for (var y = yOrigin; y < yOrigin + height; y++)
-                {
-                    BuildWall(x, y, zOrigin, Direction.South);
-                    BuildWall(x, y, zOrigin + depth, Direction.North);
-                }
-            }
-
-            // Build East/West wall
-            for (var z = zOrigin; z < zOrigin + depth; z++)
-            {
-                for (var y = yOrigin; y < yOrigin + height; y++)
-                {
-                    BuildWall(xOrigin, y, z, Direction.West);
-                    BuildWall(xOrigin + width, y, z, Direction.East);
-                }
-            }
-        }
-
-        private void BuildWall(int x, int y, int z, Direction direction)
-        {
-            // Wall by default sits on North side of origin unit square
-            switch (direction)
-            {
-                case Direction.North:
-                    Instantiate(_wall, new Vector3(x + 0.5f, y, z), Quaternion.identity);
-                    break;
-                case Direction.East:
-                    var eastWall = Instantiate(_wall, new Vector3(x, y, z + 0.5f), Quaternion.identity);
-                    eastWall.transform.localRotation = Quaternion.Euler(0, 90, 0);
-                    break;
-                case Direction.South:
-                    var southWall = Instantiate(_wall, new Vector3(x + 0.5f, y, z), Quaternion.identity);
-                    southWall.transform.localRotation = Quaternion.Euler(0, 180, 0);
-                    break;
-                case Direction.West:
-                    var westWall = Instantiate(_wall, new Vector3(x, y, z + 0.5f), Quaternion.identity);
-                    westWall.transform.localRotation = Quaternion.Euler(0, 270, 0);
-                    break;
-                default:
-                    throw new ArgumentException("Invalid direction when building walls");
-            }
+            _structureBuilder.BuildWalls(xOrigin, yOrigin, zOrigin, width, height, depth);
         }
     }
 }
---
+++ Assets/Level/Builders/StructureBuilder.cs
@@ -0,0 +1,74 @@
+﻿using Assets.World.Models;
+using System;
+using UnityEngine;
+
+namespace Assets.Level.Builders
+{
+    public class StructureBuilder : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject _ceiling;
+        [SerializeField]
+        GameObject _floor;
+        [SerializeField]
+        GameObject _wall;
+
+        public void BuildFloor(int x, int y, int z)
+        {
+            Instantiate(_floor, new Vector3(x + 0.5f, y, z + 0.5f), Quaternion.identity);
+        }
+
+        public void BuildCeiling(int x, int y, int z)
+        {
+            Instantiate(_ceiling, new Vector3(x + 0.5f, y, z + 0.5f), Quaternion.identity);
+        }
+
+        public void BuildWalls(int xOrigin, int yOrigin, int zOrigin, int width, int height, int depth)
+        {
+            // Build North/South wall
+            for (var x = xOrigin; x < xOrigin + width; x++)
+            {
+                for (var y = yOrigin; y < yOrigin + height; y++)
+                {
+                    BuildWall(x, y, zOrigin, Direction.South);
+                    BuildWall(x, y, zOrigin + depth, Direction.North);
+                }
+            }
+
+            // Build East/West wall
+            for (var z = zOrigin; z < zOrigin + depth; z++)
+            {
+                for (var y = yOrigin; y < yOrigin + height; y++)
+                {
+                    BuildWall(xOrigin, y, z, Direction.West);
+                    BuildWall(xOrigin + width, y, z, Direction.East);
+                }
+            }
+        }
+
+        public void BuildWall(int x, int y, int z, Direction direction)
+        {
+            // Wall by default sits on North side of origin unit square
+            switch (direction)
+            {
+                case Direction.North:
+                    Instantiate(_wall, new Vector3(x + 0.5f, y, z), Quaternion.identity);
+                    break;
+                case Direction.East:
+                    var eastWall = Instantiate(_wall, new Vector3(x, y, z + 0.5f), Quaternion.identity);
+                    eastWall.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                    break;
+                case Direction.South:
+                    var southWall = Instantiate(_wall, new Vector3(x + 0.5f, y, z), Quaternion.identity);
+                    southWall.transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    break;
+                case Direction.West:
+                    var westWall = Instantiate(_wall, new Vector3(x, y, z + 0.5f), Quaternion.identity);
+                    westWall.transform.localRotation = Quaternion.Euler(0, 270, 0);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid direction when building walls");
+            }
+        }
+    }
+}

e4808bececcf3a5300e9bf6740c02b2708b357f3
lrgmincher <lomy.mincher@decision.tech>
2021-12-04 13:35:29 +0000

    Fix Staircase generation

--- Assets/Level/Builders/StaircaseBuilder.cs
+++ Assets/Level/Builders/StaircaseBuilder.cs
@@ -1,10 +1,61 @@
-﻿using System.Collections;
+﻿using Assets.Helpers;
+using Assets.Level.Models;
+using Assets.World.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Level.Builders
 {
     public class StaircaseBuilder : MonoBehaviour
     {
+        [SerializeField]
+        GameObject _staircase;
 
+        public void BuildMany(ICollection<Staircase> staircases)
+        {
+            foreach (var staircase in staircases)
+            {
+                Build(staircase);
+            }
+        }
+
+        public void Build(Staircase staircase)
+        {
+            // Goes Down->Up in North direction
+            /*
+             * north: (0.5, 0, 0) rotate 180 about y
+                east: (0, 0, 0.5) rotate 270 about y
+                south: (0.5, 0, 1) rotate 0 about y
+                west: (1, 0, 0.5) rotate 90 about y
+             * 
+             */
+            var location = staircase.Previous + staircase.HorizontalOffset;
+
+            var placedStaircase = Instantiate(_staircase, location, Quaternion.identity);
+
+            switch(staircase.Direction)
+            {
+                case Direction.North:
+                    placedStaircase.transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    placedStaircase.transform.position += new Vector3(0.5f, 0, 0);
+                    break;
+                case Direction.East:
+                    placedStaircase.transform.localRotation = Quaternion.Euler(0, 270, 0);
+                    placedStaircase.transform.position += new Vector3(0, 0, 0.5f);
+                    break;
+                case Direction.South:
+                    placedStaircase.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                    placedStaircase.transform.position += new Vector3(0.5f, 0, 1);
+                    break;
+                case Direction.West:
+                    placedStaircase.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                    placedStaircase.transform.position += new Vector3(1, 0, 0.5f);
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
     }
 }
--- Assets/Level/Models/Staircase.cs
+++ Assets/Level/Models/Staircase.cs
@@ -1,24 +1,49 @@
+using Assets.World.Models;
 using UnityEngine;
 
 namespace Assets.Level.Models
 {
     public class Staircase : LevelComponent
     {
-        public Staircase(Vector3Int location, Vector3Int size) : base(location, size)
-        {
-        }
+
+        public Vector3Int Previous { get; }
+        public Vector3Int VerticalOffset { get; }
+        public Vector3Int HorizontalOffset { get; }
+        public bool IsUp { get; }
+        public Direction Direction { get; }
 
         public Staircase(Vector3Int previous, Vector3Int verticalOffset, Vector3Int horizontalOffset) : base()
         {
-            var first = new LevelComponentPiece(Id, previous + horizontalOffset, null, previous + horizontalOffset * 2);
-            var second = new LevelComponentPiece(Id, previous + horizontalOffset * 2, first, previous + verticalOffset + horizontalOffset);
-            var third = new LevelComponentPiece(Id, previous + verticalOffset + horizontalOffset, second, previous + verticalOffset + horizontalOffset * 2);
-            var fourth = new LevelComponentPiece(Id, previous + verticalOffset + horizontalOffset * 2, third, null);
+            AddPieces(previous, verticalOffset, horizontalOffset);
+
+            var isUp = verticalOffset.y > 0;
+            var direction = horizontalOffset.x == 0 ? horizontalOffset.z > 0 ? Direction.North : Direction.South
+                                                    : horizontalOffset.x > 0 ? Direction.East : Direction.West;
+
+            Previous = previous;
+            VerticalOffset = verticalOffset;
+            HorizontalOffset = horizontalOffset;
+
+            IsUp = isUp;
+            Direction = direction;
+        }
+
+        private void AddPieces(Vector3Int previous, Vector3Int verticalOffset, Vector3Int horizontalOffset)
+        {
+            var first = new LevelComponentPiece(Id, previous + horizontalOffset, previous + horizontalOffset * 2);
+            var second = new LevelComponentPiece(Id, previous + horizontalOffset * 2, previous + verticalOffset + horizontalOffset, ref first);
+            var third = new LevelComponentPiece(Id, previous + verticalOffset + horizontalOffset, previous + verticalOffset + horizontalOffset * 2, ref second);
+            var fourth = new LevelComponentPiece(Id, previous + verticalOffset + horizontalOffset * 2, null, ref third);
 
             AddPiece(first);
             AddPiece(second);
             AddPiece(third);
             AddPiece(fourth);
+        }
+
+        public Staircase(Vector3Int fromDirection, Vector3Int toDirection)
+        {
+
         }
     }
 }

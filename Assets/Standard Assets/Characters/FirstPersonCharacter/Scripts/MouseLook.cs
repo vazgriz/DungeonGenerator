be5311906457b314d0ecca7973c8ba7052719c45
Lomy <lrgmincher@gmail.com>
2022-05-07 12:52:37 +0100

    Use simplified approach to mouse look. Rather than setting rotation/angles, just check mouse delta, set camera pitch using Y, transform.Rotate using X This means we don't need to use 2 versions of the script for player and camera. Also don't need to worry about input delay For reference: https://www.youtube.com/watch?v=PmIPqGqp8UY

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -3,48 +3,30 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [Flags]
-    public enum RotationDirection
+    [SerializeField]
+    private Transform _playerCamera;
+
+    [SerializeField]
+    private bool _lockCursor = true;
+
+    [SerializeField]
+    private float MouseSensitivity = 3.5f;
+
+    private float _cameraPitch = 0.0f;
+
+    private void Start()
     {
-        None,
-        Horizontal = 1 << 0,
-        Vertical = 1 << 1
+        if (_lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
-
-    [Tooltip("A multiplier to the input. Describes the maximum speed in degrees/second. To flip vertical rotation, set Y to a negative value")]
-    [SerializeField]
-    private Vector2 Sensitivity;
-
-    [Tooltip("The rotation acceleration, in degrees/second")]
-    [SerializeField]
-    private Vector2 Acceleration;
-
-    [Tooltip("The period to wait until resetting the input value. Should be set as low as possible while avoiding stuttering")]
-    [SerializeField]
-    private float InputLagPeriod;
-
-    [Tooltip("Which directions this object can rotate")]
-    [SerializeField]
-    private RotationDirection _rotationDirections;
-
-    private Vector2 _currentRotation;
-    private Vector2 _currentVelocity;
-
-    private Vector2 _lastInputEvent;
-    private float _inputLagTimer;
 
     private void OnEnable()
     {
-        ResetState();
         var cameraAngles = GetCurrentCameraAngles();
         SetLocalRotation(cameraAngles);
-
-        void ResetState()
-        {
-            _currentVelocity = default;
-            _inputLagTimer = default;
-            _lastInputEvent = default;
-        }
 
         Vector3 GetCurrentCameraAngles()
         {
@@ -66,60 +48,24 @@ public class MouseLook : MonoBehaviour
         void SetLocalRotation(Vector3 euler)
         {
             transform.localEulerAngles = euler;
-            _currentRotation = new Vector2(euler.y, euler.x);
         }
     }
 
     private void Update()
     {
-        UpdateCurrentRotation();
-        transform.localEulerAngles = GetDesiredTransform();
+        var mouseDelta = GetMouseInput();
+
+        _cameraPitch -= mouseDelta.y * MouseSensitivity;
+        _cameraPitch = ClampVerticalAngle(_cameraPitch);
+
+        _playerCamera.localEulerAngles = Vector3.right * _cameraPitch;
+        transform.Rotate(Vector3.up * mouseDelta.x * MouseSensitivity);
     }
 
-    private void UpdateCurrentRotation()
-    {
-        _currentVelocity = GetDesiredVelocity();
-        _currentRotation += _currentVelocity * Time.deltaTime;
-        _currentRotation.y = ClampVerticalAngle(_currentRotation.y);
-    }
-
-    private Vector2 GetDesiredVelocity()
-    {
-        var maximumVelocity = GetInputVector() * Sensitivity;
-
-        if ((_rotationDirections & RotationDirection.Horizontal) == 0)
-        {
-            maximumVelocity.x = 0;
-        }
-
-        if ((_rotationDirections & RotationDirection.Vertical) == 0)
-        {
-            maximumVelocity.y = 0;
-        }
-
-        return new Vector2(
-            Mathf.MoveTowards(_currentVelocity.x, maximumVelocity.x, Acceleration.x * Time.deltaTime),
-            Mathf.MoveTowards(_currentVelocity.y, maximumVelocity.y, Acceleration.y * Time.deltaTime));
-    }
-
-    private Vector3 GetDesiredTransform() => new Vector3(_currentRotation.y, _currentRotation.x, 0);
-
-    private Vector2 GetInputVector()
-    {
-        _inputLagTimer += Time.deltaTime;
-
+    private Vector2 GetMouseInput()
+    { 
         var input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-
-        if (InputIsNonZero() || InputLagTimeHasElapsed())
-        {
-            _lastInputEvent = input;
-            _inputLagTimer = 0;
-        }
-
-        return _lastInputEvent;
-
-        bool InputIsNonZero() => !(Mathf.Approximately(0, input.x) && Mathf.Approximately(0, input.y));
-        bool InputLagTimeHasElapsed() => _inputLagTimer > InputLagPeriod;
+        return input;
     }
 
     private float ClampVerticalAngle(float angle)

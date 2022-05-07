1c9cc9e2749e144f59a1c1b79c9532b06ae69477
Lomy <lrgmincher@gmail.com>
2022-05-07 11:37:11 +0100

    Basic mouse look with customisable sensitivity, acceleration and input lag delay

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -1,115 +1,62 @@
-using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
-namespace UnityStandardAssets.Characters.FirstPerson
+public class MouseLook : MonoBehaviour
 {
-    [Serializable]
-    public class MouseLook
+    [Tooltip("A multiplier to the input. Describes the maximum speed in degrees/second. To flip vertical rotation, set Y to a negative value")]
+    [SerializeField]
+    private Vector2 Sensitivity;
+
+    [Tooltip("The rotation acceleration, in degrees/second")]
+    [SerializeField]
+    private Vector2 Acceleration;
+
+    [Tooltip("The period to wait until resetting the input value. Should be set as low as possible while avoiding stuttering")]
+    [SerializeField]
+    private float InputLagPeriod;
+
+    private Vector2 _currentRotation;
+    private Vector2 _currentVelocity;
+
+    private Vector2 _lastInputEvent;
+    private float _inputLagTimer;
+
+    private void Update()
     {
-        public float XSensitivity = 2f;
-        public float YSensitivity = 2f;
-        public bool clampVerticalRotation = true;
-        public float MinimumX = -90F;
-        public float MaximumX = 90F;
-        public bool smooth;
-        public float smoothTime = 5f;
-        public bool lockCursor = true;
+        UpdateCurrentRotation();
+        transform.localEulerAngles = GetDesiredTransform();
+    }
 
+    private void UpdateCurrentRotation()
+    {
+        _currentVelocity = GetDesiredVelocity();
+        _currentRotation += _currentVelocity * Time.deltaTime;
+    }
 
-        private Quaternion m_CharacterTargetRot;
-        private Quaternion m_CameraTargetRot;
-        private bool m_cursorIsLocked = true;
+    private Vector2 GetDesiredVelocity()
+    {
+        var maximumVelocity = GetInputVector() * Sensitivity;
+        return new Vector2(
+            Mathf.MoveTowards(_currentVelocity.x, maximumVelocity.x, Acceleration.x * Time.deltaTime),
+            Mathf.MoveTowards(_currentVelocity.y, maximumVelocity.y, Acceleration.y * Time.deltaTime));
+    }
 
-        public void Init(Transform character, Transform camera)
+    private Vector3 GetDesiredTransform() => new Vector3(_currentRotation.y, _currentRotation.x, 0);
+
+    private Vector2 GetInputVector()
+    {
+        _inputLagTimer += Time.deltaTime;
+
+        var input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        if (InputIsNonZero() || InputLagTimeHasElapsed())
         {
-            m_CharacterTargetRot = character.localRotation;
-            m_CameraTargetRot = camera.localRotation;
+            _lastInputEvent = input;
+            _inputLagTimer = 0;
         }
 
+        return _lastInputEvent;
 
-        public void LookRotation(Transform character, Transform camera)
-        {
-            float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
-            float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
-
-            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
-            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
-
-            if(clampVerticalRotation)
-                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
-
-            if(smooth)
-            {
-                character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
-                    smoothTime * Time.deltaTime);
-                camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
-                    smoothTime * Time.deltaTime);
-            }
-            else
-            {
-                character.localRotation = m_CharacterTargetRot;
-                camera.localRotation = m_CameraTargetRot;
-            }
-
-            UpdateCursorLock();
-        }
-
-        public void SetCursorLock(bool value)
-        {
-            lockCursor = value;
-            if(!lockCursor)
-            {//we force unlock the cursor if the user disable the cursor locking helper
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-        }
-
-        public void UpdateCursorLock()
-        {
-            //if the user set "lockCursor" we check & properly lock the cursos
-            if (lockCursor)
-                InternalLockUpdate();
-        }
-
-        private void InternalLockUpdate()
-        {
-            if(Input.GetKeyUp(KeyCode.Escape))
-            {
-                m_cursorIsLocked = false;
-            }
-            else if(Input.GetMouseButtonUp(0))
-            {
-                m_cursorIsLocked = true;
-            }
-
-            if (m_cursorIsLocked)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-            else if (!m_cursorIsLocked)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-        }
-
-        Quaternion ClampRotationAroundXAxis(Quaternion q)
-        {
-            q.x /= q.w;
-            q.y /= q.w;
-            q.z /= q.w;
-            q.w = 1.0f;
-
-            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
-
-            angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
-
-            q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
-
-            return q;
-        }
-
+        bool InputIsNonZero() => !(Mathf.Approximately(0, input.x) && Mathf.Approximately(0, input.y));
+        bool InputLagTimeHasElapsed() => _inputLagTimer > InputLagPeriod;
     }
 }
